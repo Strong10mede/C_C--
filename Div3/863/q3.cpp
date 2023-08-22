a8e555843863b0691fd9f576514cf29eb4361186
Mayur Kumar <kmayur809@gmail.com>
2023-08-22 15:30:44 +0530

    863

---
+++ Div3/863/q3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define SORT(a) sort(a.begin(), a.end())
+#define FOR(a,b, n) for (ll a = b; a < n;a++)
+#define vi vector<ll>
+#define vii vector<vector<ll>>
+#define pii pair<ll,ll>
+using namespace std;
+
+void sol(){
+    
+}
+int main(){
+    int test;
+    cin >> test;
+    while(test--){
+        sol();
+    }
+    return 0;
+}
