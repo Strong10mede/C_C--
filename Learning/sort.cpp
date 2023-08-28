f37d64697c86fe96a5a43fc483bcdf969b71a8c8
Mayur Kumar <kmayur809@gmail.com>
2023-08-28 16:09:46 +0530

    commit

--- Learning/sort.cpp
+++ Learning/sort.cpp
@@ -72,8 +72,35 @@ for (int i = 1; i < n;i++){
     }
 }
 
+void heapify(vector<int> &a, int n, int i){
+    int b = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if(l<n && a[b]<a[l]){
+        b = l;
+    }
+
+    if(r<n && a[b]<a[r]){
+        b = r;
+    }
+
+    if(b != i){
+        swap(a[b], a[i]);
+        heapify(a, n, b);
+    }
+}
 void heap(vector<int> &a){
-    
+    int n = a.size();
+    for (int i = n / 2 - 1; i >= 0;i--){
+        heapify(a, n, i);
+    }
+
+    for (int i = n - 1; i >= 0;i--){
+        std::swap(a[0], a[i]);
+        heapify(a, i, 0);
+    }
+
 };
 
 int partition(vector<int> &a, int l,int r){
